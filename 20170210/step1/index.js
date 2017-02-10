a3cc0c7516d52d10e07fc88d516490cfdc811ba6
Hiroto Murai <murai_hiroto@liginc.co.jp>
2017-02-10 11:54:56 +0900

    Add 20170210

---
+++ 20170210/step1/index.js
@@ -0,0 +1,11 @@
+// index.js
+const fs   = require('fs')
+
+const args = process.argv
+
+let target = args[2]
+
+fs.readFile(target, 'utf-8', (err, str) => {
+  if (err) throw err
+  console.log(str)
+})
---
+++ 20170210/step2/filecopy.js
@@ -0,0 +1,9 @@
+// filecopy.js
+const fs   = require('fs-extra')
+
+const args = process.argv
+
+let from = args[2]
+let to   = args[3]
+
+fs.copy(from, to)
